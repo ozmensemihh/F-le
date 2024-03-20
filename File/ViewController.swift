f47684940534ee795f2fbf446bea831ca1d574ac
semih <ozmensemih90@gmail.com>
2024-03-20 03:16:29 +0300

    dosya okuma, yazma, silme işlemleri yazıldı.

--- File/ViewController.swift
+++ File/ViewController.swift
@@ -8,12 +8,84 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var txtGirdi: UITextField!
+    
+    @IBOutlet weak var imgResim: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func dosyaYaz(_ sender: Any) {
+        
+        let message = txtGirdi.text
+        
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            let dosyaYolu = dir.appendingPathComponent("dosyam.txt")
+                   
+            do{
+                try message?.write(to: dosyaYolu, atomically: false, encoding: .utf8)
+                txtGirdi.text?.removeAll()
+                
+            }catch{
+                print("Dosya yazarken hata oldu")
+            }
+            
+        }
+        
+        
+    }
+    @IBAction func dosyaOku(_ sender: UIButton) {
+        
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            let dosyaYolu = dir.appendingPathComponent("dosyam.txt")
+            if FileManager.default.fileExists(atPath: dosyaYolu.path){
+                do{
+                    txtGirdi.text = try String(contentsOf: dosyaYolu, encoding: .utf8)
+                }catch{
+                    print("Dosya okurken hata oldu")
+                }
+            }
+            
+        }
+        
+    }
+    
+    @IBAction func dosyaSil(_ sender: UIButton) {
+        
+        
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            
+            let dosyaYolu = dir.appendingPathComponent("dosyam.txt")
+            
+            if FileManager.default.fileExists(atPath: dosyaYolu.path) {
+                do{
+                    try FileManager.default.removeItem(at: dosyaYolu)
+                    txtGirdi.text?.removeAll()
+                    
+                }catch{
+                    print("Dosya yazarken hata oldu")
+                }
+            }
+            
+                   
+           
+            
+        }
+        
+    }
+    
+    
+    @IBAction func resimKaydet(_ sender: Any) {
+    }
+    
+    @IBAction func resimGöster(_ sender: UIButton) {
+    }
+    
+    @IBAction func resimSil(_ sender: Any) {
+    }
+    
 }
 
